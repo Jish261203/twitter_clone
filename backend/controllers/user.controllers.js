2b1c5beaa47af451dc40a91e1a9ea8bec4bd2f83
Your Name <jish261203@gmail.comgit config --global user.name Jishnay>
2024-06-11 11:08:23 +0530

    Notification Routes and COntrollers Added

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -42,14 +42,6 @@ export const followUnfollowUser = async (req, res) => {
 			await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
 			await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
 
-			// const newNotification = new Notification({
-			//     type: "unfollow",
-			//     from: req.user._id,
-			//     to:userToModify._id
-			// })
-
-			// await newNotification.save()
-
 			res.status(200).json({ message: "User unfollowed successfully" });
 		} else {
 			await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
