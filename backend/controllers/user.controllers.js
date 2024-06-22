f682784b444c8070ffdc357e4449fdf3acd34ced
Your Name <jish261203@gmail.comgit config --global user.name Jishnay>
2024-06-22 23:59:13 +0530

    Updated user profile in edit profile added

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -95,7 +95,7 @@ export const updateUser = async (req, res) => {
 	const { fullName, email, username, currentPassword, newPassword, bio, link } =
 		req.body;
 
-	const { profileImg, coverImg } = req.body;
+	let { profileImg, coverImg } = req.body;
 
 	const userId = req.user._id;
 
